75b4c2694c29d8cf2fe539a03f8ecc1e9a70da94
Kashiwade-music <music5d7_9265@outlook.jp>
2023-05-25 09:15:28 +0000

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# install oh-my-posh
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+
+# touch and write pwsh $PROFILE
+mkdir -p ~/.config/powershell
+touch ~/.config/powershell/profile.ps1
+echo "oh-my-posh init pwsh --config $(brew --prefix oh-my-posh)/themes/night-owl.omp.json | Invoke-Expression" >>~/.config/powershell/profile.ps1
