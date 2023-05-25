c34dccf8f31ebb9121baa6efb49fe0f05fd6b309
Kashiwade-music <music5d7_9265@outlook.jp>
2023-05-25 09:50:14 +0000

    edit profile

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,4 +6,5 @@ brew install jandedobbeleer/oh-my-posh/oh-my-posh
 # touch and write pwsh $PROFILE
 mkdir -p ~/.config/powershell
 touch ~/.config/powershell/profile.ps1
-echo "oh-my-posh init pwsh --config $(brew --prefix oh-my-posh)/themes/night-owl.omp.json | Invoke-Expression" >>~/.config/powershell/profile.ps1
+echo "oh-my-posh init pwsh --config $(brew --prefix oh-my-posh)/themes/night-owl.omp.json | Invoke-Expression" >> ~/.config/powershell/profile.ps1
+echo '$VTR_ROOT="/workspace"' >> ~/.config/powershell/profile.ps1
